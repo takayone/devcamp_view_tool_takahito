4353b3b39903dbb081351d29096c203a95838fbc
taka yone <tkhtynd1991@gmail.com>
2019-01-15 21:53:14 +0900

    inital

---
+++ lib/devcamp_view_tool_takahito.rb
@@ -0,0 +1,7 @@
+require "devcamp_view_tool_takahito/version"
+require "devcamp_view_tool_takahito/renderer"
+
+module DevcampViewToolTakahito
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/devcamp_view_tool_takahito/version.rb
@@ -0,0 +1,3 @@
+module DevcampViewToolTakahito
+  VERSION = "0.1.0"
+end
